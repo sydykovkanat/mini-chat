c6edecf32d5ec680c31cd22203354cd1a5b76c75
sydykovkanat <kanatraccoon@gmail.com>
2025-02-06 16:57:30 +0600

    some changes

--- backend/src/chat/chat-gateway.ts
+++ backend/src/chat/chat-gateway.ts
@@ -1,35 +1,17 @@
-import {
-  MessageBody,
-  OnGatewayConnection,
-  OnGatewayDisconnect,
-  SubscribeMessage,
-  WebSocketGateway,
-  WebSocketServer,
-} from '@nestjs/websockets';
-import { Server, Socket } from 'socket.io';
+import { MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { Server } from 'socket.io';
 
 @WebSocketGateway(8001, { cors: { origin: '*' } })
-export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
+export class ChatGateway {
   @WebSocketServer() server: Server;
-
-  handleConnection(client: Socket) {
-    console.log(`client ${client.id} connected`);
-
-    this.server.emit('user-connected', {
-      message: `client ${client.id} connected`,
-    });
-  }
-
-  handleDisconnect(client: Socket) {
-    console.log(`client ${client.id} disconnected`);
-
-    this.server.emit('user-disconnected', {
-      message: `client ${client.id} disconnected`,
-    });
-  }
 
   @SubscribeMessage('messages')
   handleMessage(@MessageBody() message: string) {
     this.server.emit('messages', { message });
+  }
+
+  @SubscribeMessage('user-connected')
+  handleUserConnected(@MessageBody() username: string) {
+    this.server.emit('user-connected', { username });
   }
 }
