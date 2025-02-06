6bebbbd5b63526478afebe2e5d5e7473aa9fd866
sydykovkanat <kanatraccoon@gmail.com>
2025-02-06 14:28:52 +0600

    init chat app and frontend

---
+++ backend/src/chat/chat-gateway.ts
@@ -0,0 +1,35 @@
+import {
+  MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+
+@WebSocketGateway(3002, { cors: { origin: '*' } })
+export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
+  @WebSocketServer() server: Server;
+
+  handleConnection(client: Socket) {
+    console.log(`client ${client.id} connected`);
+
+    this.server.emit('user-connected', {
+      message: `client ${client.id} connected`,
+    });
+  }
+
+  handleDisconnect(client: Socket) {
+    console.log(`client ${client.id} disconnected`);
+
+    this.server.emit('user-disconnected', {
+      message: `client ${client.id} disconnected`,
+    });
+  }
+
+  @SubscribeMessage('messages')
+  handleMessage(@MessageBody() message: string) {
+    this.server.emit('messages', { message });
+  }
+}
---
+++ frontend/src/app.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Button } from '@/components/ui/button';
+
+export const App: React.FC = () => {
+  return <Button size={'sm'}>Click me</Button>;
+};
